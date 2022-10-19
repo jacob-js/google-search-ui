f9b7fa10ea542acc9adf5a514a99cd8c2e98d9bd
Jacob Dev <mercihabam@gmail.com>
2022-10-19 09:06:14 +0200

    remove abstract classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,25 @@ module.exports = {
         'roboto': "'Roboto', sans-serif"
       },
       width: {
-        'like-full': '90%'
+        'like-full': '90%',
+        '10v': '100vh',
+        '8v': '80vh'
+      },
+      height: {
+        '10v': '100vh',
+        '8v': '80vh'
+      },
+      minHeight: {
+        '10v': '100vh',
+        '8v': '80vh'
+      },
+      minWidth: {
+        'like-full': '90%',
+        '10v': '100vh',
+        '8v': '80vh'
+      },
+      padding: {
+        '2.5': 'calc(1rem - 6px)'
       }
     },
   },
